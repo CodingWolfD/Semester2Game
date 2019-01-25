7c4b81d53248e268a1ba97fff02d7fc2a6f55045
daniel martin <danielmartin1444@gmail.com>
2019-01-25 14:31:49 +0000

    Added new classes

---
+++ Semester2Game/src/com/Main/Game.java
@@ -0,0 +1,46 @@
+package com.Main;
+
+import javax.swing.JFrame;
+
+public class Game
+{
+	public static final int WINDOW_WIDTH = 800;
+	public static final int WINDOW_HEIGHT = 384;
+	
+	private static final String TITLE = "New and Improved Game";
+	
+	private JFrame mainWindow;
+	private LevelPanel lp;
+	
+	public Game()
+	{
+		initGame();
+		initComponents();
+		initWindow();
+	}
+	
+	private void initGame()
+	{
+		lp = new LevelPanel();
+		mainWindow = new JFrame();
+	}
+	
+	private void initComponents()
+	{
+		mainWindow.add(lp);
+	}
+	
+	private void initWindow()
+	{
+		mainWindow.setTitle(TITLE);
+		mainWindow.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindow.setVisible(true);
+	}
+	
+	public static void main(String[] args)
+	{
+		Game g = new Game()
+	}
+	
+}
---
+++ Semester2Game/src/com/Main/LevelPanel.java
@@ -0,0 +1,91 @@
+package com.Main;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+public class LevelPanel extends JPanel implements KeyListener
+{
+	public static final int PANEL_WIDTH = Game.WINDOW_WIDTH - 10;
+	public static final int PANEL_HEIGHT = Game.WINDOW_HEIGHT - 10;
+	
+	private final double FPS = 60;
+	private final double TARGET_UPDATE_TIME = 1000/FPS;
+	
+	private BufferedImage screenBuffer;
+	private Graphics2D graphics;
+	
+	private Thread gameLoop = null;
+	
+	private boolean isRunning;
+	private boolean isPaused;
+	
+	private LevelManager lm;
+	
+	public LevelPanel()
+	{
+		super();
+		
+		isRunning = true;
+		isPaused = false;
+		
+		lm = new LevelManager();
+	}
+	
+	public void startGame()
+	{
+		initGraphics();
+		
+		gameLoop = new Thread()
+		{
+			public void run()
+			{
+				gameLoop();
+			}
+		};
+		
+		gameLoop.start();
+	}
+	
+	private void gameLoop()
+	{
+		double startTime;
+		double finishTime;
+		double deltaT;
+		double waitT;
+		double fps;
+		
+		while(isRunning)
+		{
+			if(!isPaused)
+			{
+				lm.Update();
+				lm.updateScreenBuffer(graphics);
+				
+				repaint();
+				
+				finishTime = System.nanoTime();
+				
+				deltaT = finishTime - startTime;
+				
+				waitT = TARGET_UPDATE_TIME - deltaT / 1000000;
+				
+				if(waitT < 5)
+				{
+					waitT = 5;
+				}
+				
+				try
+				{
+					Thread.sleep((long) waitT);
+				}
+				catch(InterruptedException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
